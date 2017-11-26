c592230eb18bde478722383bdae1627445c7a32d
Frost <matthias.prost@epitech.eu>
2017-11-26 22:53:59 +0100

    [+] Unit test

--- units/test.c
+++ units/test.c
@@ -1,21 +1,14 @@
 /**
- * @Author: Remi Gastaldi
- * @Date:   2017-10-05T12:45:12+02:00
- * @Project: Spider
- * @Last modified by:   gastal_r
- * @Last modified time: 2017-11-22T19:20:07+01:00
- */
-
+* @Author: Remi Gastaldi
+* @Date:   2017-10-05T12:45:12+02:00
+* @Project: Spider
+ * @Last modified by:   prost_m
+ * @Last modified time: 2017-11-26T22:53:44+01:00
+*/
 
 #include <criterion/criterion.h>
+#include "unistd.h"
 
-
-Test(base_mult, mult_2_by_5)
-{
-  cr_assert_str_eq("10", "10");
-}
-
-Test(base_mult, mult_20_by_100000)
-{
-  cr_assert_str_eq("100000", "2000000");
+Test(sample, library_exist) {
+  cr_assert_neq(-1, access("/lib/x86_64-linux-gnu/security/mypam.so", F_OK), "Library mypam.so doesn't exist");
 }
